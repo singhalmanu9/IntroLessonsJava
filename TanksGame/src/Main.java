6f2e22bae8baa3992b98c7b0e16e6100228592c9
singhalmanu9 <singhalmanu9@gmail.com>
2014-11-27 11:54:19 -0500

    Added the main class

---
+++ TanksGame/src/Main.java
@@ -0,0 +1,14 @@
+
+/**
+ * This is the main class for the TanksGame project.
+ * @author Manu S.
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
